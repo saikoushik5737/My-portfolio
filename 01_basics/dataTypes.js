8de26407ddbfdad838bb5f1977fb155e82f1d532
Krishna Sai Kousik Pavani <96521387+saikoushik5737@users.noreply.github.com>
2025-02-12 04:24:12 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treats all JS code as newer version
+
+// alert(3+3) //We are using nodeJs, not browser (alert only works in browser)
+
+console.log(3+4);
+
+console.log("Koushik");
+
+let name = "Krishna"
+let age = 25
+let isLoggedIn = true
+let state = null
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+/*
+number => 2 power 35
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+Symbol => unique
+Object
+*/
