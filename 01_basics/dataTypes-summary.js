1ba49041e5ad55820bb6e1072f5e0a28b39f5294
Krishna Sai Kousik Pavani <96521387+saikoushik5737@users.noreply.github.com>
2025-02-17 10:15:16 +0000

    DataType Summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,43 @@
+// Primitive type
+
+// 7 types : String, Number, Boolean, null, Undefined, Symbol, Bigint
+
+const score = 100
+const scoreValue = 100.3
+
+
+const isLoggedIn = false
+const outsideTemperature = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 234553256789865478n
+
+// console.log(typeof bigNumber);
+
+
+
+// Reference type (Non primitive)
+
+// Array, Objects, Functions
+
+//Array
+const heroes = ["shakthiman", "nagraaj", "doga"]
+
+//Objects
+let myObj= {
+    name: "Koushik",
+    age: 22
+}
+
+//Functions
+const myFunction = function() {
+    console.log("Hello World");
+}
+
+console.log(typeof anotherId);
