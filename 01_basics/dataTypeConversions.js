9c8aa68bc0b28a7f349aac929241ef0e4efacd20
Krishna Sai Kousik Pavani <96521387+saikoushik5737@users.noreply.github.com>
2025-02-17 09:05:11 +0000

    Conversions and operations

--- 01_basics/dataTypeConversions.js
+++ 01_basics/dataTypeConversions.js
@@ -17,12 +17,41 @@ true => 1; false => 0
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /*
 1 => true; 0 => false
 "" => false
 "siva" => true
 */
+
+// **************************** Operations *********************************** //
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 = "Hello"
+let str2 = " Siva"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
 
