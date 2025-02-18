ad80b7ec2325cd1844996e9489d44d9f47902def
Krishna Sai Kousik Pavani <96521387+saikoushik5737@users.noreply.github.com>
2025-02-18 05:09:37 +0000

    math n nums

---
+++ 01_basics/nums-and-maths.js
@@ -0,0 +1,32 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number (100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 233.8965
+// console.log(otherNumber.toPrecision(5));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString());
+
+//+++++++++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++//
+
+// console.log(Math);
+// console.log(Math.abs(-20));
+// console.log(Math.round(86.4));
+// console.log(Math.ceil(86.2));
+// console.log(Math.floor(86.2));
+// console.log(Math.min(2,3,4,5,6));
+// console.log(Math.max(2,3,4,5,6));
+
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
