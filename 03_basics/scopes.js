84cfa45df176940ea52f2dfc3b8c81f035de9484
Krishna Sai Kousik Pavani <96521387+saikoushik5737@users.noreply.github.com>
2025-02-27 23:45:34 +0000

    Nested scope and Hoisting

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -5,11 +5,55 @@ if (true) {
     let a = 10;
     const b = 20;
     // c = 30;
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+//******************NESTED SCOPE*********************//
+
+function one() {
+    const userName = "Sai"
+
+    function two() {
+        const website = "youtube"
+        console.log(userName);    
+    }
+    // console.log(website); 
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const userName = "Sai"
+    if (userName === "Sai"){
+        const website = " Youtube"
+        // console.log(userName + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(userName);
+
+
+//*****************HOISTING*****************//
+
+console.log(addone(5));
+
+function addone(num) { //FUNCTION DECLARATION
+    return num+1;
+}
+
+
+// addTwo(5)
+
+const addTwo =  function addtwo(num) { //FUNCTION EXPRESSION    
+    return num+2
+}
 
